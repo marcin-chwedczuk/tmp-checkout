8e394e85df1367f823271c09813376e8b31dec8e
Marcin Chwedczuk <chwedczukm@yahoo.com>
2017-10-28 09:47:28 +0200

    Add project skel generated by Spring Initializr.

---
+++ src/main/java/pl/marcinchwedczuk/checkout3/Checkout3Application.java
@@ -0,0 +1,12 @@
+package pl.marcinchwedczuk.checkout3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkout3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkout3Application.class, args);
+	}
+}
